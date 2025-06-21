249b47ad3ff8c0f1bd93913a551a1717d29d5036
mma3212 <mma3212@MMA3212P1.na.mmfg.net>
2025-06-21 17:40:33 +0530

    just kidding

--- recalling/src/main/java/com/recall/recalling/hello/Hello.java
+++ recalling/src/main/java/com/recall/recalling/hello/Hello.java
@@ -8,7 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-//new changes
+//new changes -1
 @Controller
 public class Hello {
 	
